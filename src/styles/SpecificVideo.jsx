a1ea7dc600a35f2894f1f2b43ee907148d302953
healthmudraaa <common.healthmudraa@gmail.com>
2024-06-11 20:19:14 +0530

    Revert "Updated Specific Video Page"

--- src/styles/SpecificVideo.jsx
+++ src/styles/SpecificVideo.jsx
@@ -10,30 +10,12 @@ export const SpecificVideoWrapper = styled(Flex)`
   .leadFormWrapper {
     width: 30%;
     .leadForm {
-      margin-top:30px;
       position: sticky !important;
       top: 20px;
       right: 0;
       width: 100%;
     }
   }
-  .qa-item {
-    border-width: 0px;
-    border-bottom-width: 1px;
-    border-style: solid;
-    border-color: rgb(209 213 219);
-    margin-top: 20px;
-  }
-  .question-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    cursor: pointer;
-  }
-  .question{
-    margin-top:5px;
-  }
-
   @media screen and (max-width: 800px) {
     flex-direction: column;
     .videoDetails {
@@ -41,61 +23,8 @@ export const SpecificVideoWrapper = styled(Flex)`
     }
     .leadFormWrapper {
       width: 100%;
-    }
-    .leadForm {
       display: none;
     }
-    
-  }
-
-  .h1{
-    margin-top: 3%;
-  }
-  .h3{
-    font-size: 20px;
-    font-weight: 700;
-  }
-  /* PC Screen */
-  @media only screen and (min-width: 1024px) {
-    .h1{
-      font-size: 45px;
-      font-weight: 700;
-    }
-    .h2{
-      font-size: 30px;
-      font-weight: 700;
-      letter-spacing: 0.1em;
-    }
-  }
-
-  /* Tablet screens */
-  @media only screen and (min-width: 768px) and (max-width: 1023px) {
-    .h1{
-      font-size: 30px;
-      font-weight: 700;
-    }
-    .h2{
-      font-size: 30px;
-      font-weight: 700;
-      letter-spacing: 0.1em;
-    } 
-  }
-
-  /* Mobile screens */
-  @media only screen and (max-width: 767px) {
-    .h1{
-      font-size: 25px;
-      font-weight: 700;
-    }
-    .h2{
-      font-size: 23px;
-      font-weight: 700;
-      letter-spacing: 0.025em;
-    }
-    .h3{
-      font-size: 17px;
-      font-weight: 600;
-    } 
   }
 `;
 
@@ -114,5 +43,3 @@ export const BookAppointmentFixedbar = styled(Flex)`
     display: none;
   }
 `;
-
-
