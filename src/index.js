f1d149ca3e0d99401b6ab494f0cb7cb30db4819f
soham <jambhwadekarsoham@gmail.com>
2024-07-07 13:28:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { Toaster } from "react-hot-toast";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
