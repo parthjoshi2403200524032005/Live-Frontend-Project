424c19537ccb7d7fe0d75160b73f6917e89ec107
soham <jambhwadekarsoham@gmail.com>
2024-07-08 19:50:10 +0530

    Update VideoFetch.jsx

--- src/DoctorPannel/DoctorComponents/VideoFetch.jsx
+++ src/DoctorPannel/DoctorComponents/VideoFetch.jsx
@@ -14,13 +14,14 @@ import {
   FormControl,
   MenuItem,
   Select,
+  Button,
+  Chip
 } from "@mui/material";
 
 import Plyr from "plyr-react";
 import { UploadButton } from "../../CustomStyles/Styles";
 import {
   doctorDetailsGet,
-  doctorDetailsUpdate,
   getDoctorVideos,
   createDoctorVideo,
   deleteDoctorVideo,
@@ -49,14 +50,14 @@ const VideoFetch = () => {
     },
   });
 
-  const [videoInfo, setVideoInfo] = useState({
-    link: "",
-    title: "",
-    description: "",
-    category: "",
-  });
+  // const [videoInfo, setVideoInfo] = useState({
+  //   link: "",
+  //   title: "",
+  //   description: "",
+  //   category: "",
+  // });
 
-  const [categories, setCategories] = useState([
+  const [categories] = useState([
     "General",
     "Brain and Nerves",
     "Bones and Muscles",
@@ -78,6 +79,115 @@ const VideoFetch = () => {
     "Ear Nose Throat",
     "Acupressure therapy",
   ]);
+
+
+  const [videoInfo, setVideoInfo] = useState({
+    link: "",
+    title: "",
+    description: "",
+    category: "",
+    keywords: [],
+    questionsAnswers: []
+  });
+
+  const [currentKeyword, setCurrentKeyword] = useState("");
+  const [currentQuestion, setCurrentQuestion] = useState("");
+  const [currentAnswer, setCurrentAnswer] = useState("");
+
+  const handlePost = async () => {
+    if (videoInfo.title && videoInfo.link) {
+      const responseJson = await createDoctorVideo(videoInfo);
+      if (responseJson.data.status) {
+        toast.success(responseJson.data.message);
+        getDoctorDetails();
+        setVideoInfo({
+          link: "",
+          title: "",
+          description: "",
+          category: "",
+          keywords: [],
+          questionsAnswers: []
+        });
+      } else {
+        toast.error(responseJson.data.message);
+      }
+    }
+  };
+
+  const deleteVideo = async (id) => {
+    const responseJson = await deleteDoctorVideo(id);
+    if (responseJson.data.status) {
+      toast.success(responseJson.data.message);
+      getDoctorDetails();
+    } else {
+      toast.error(responseJson.data.message);
+    }
+  };
+
+  const updateVideo = async () => {
+    const responseJson = await updateDoctorVideo(videoInfo._id, videoInfo);
+    if (responseJson.data.status) {
+      toast.success(responseJson.data.message);
+      setVideoInfo({
+        link: "",
+        title: "",
+        description: "",
+        category: "",
+        keywords: [],
+        questionsAnswers: []
+      });
+      getDoctorDetails();
+    } else {
+      toast.error(responseJson.data.message);
+    }
+  };
+
+  const handleAddKeyword = () => {
+    if (currentKeyword.trim() !== "") {
+      setVideoInfo({
+        ...videoInfo,
+        keywords: [...videoInfo.keywords, currentKeyword]
+      });
+      setCurrentKeyword("");
+    }
+  };
+
+  const handleDeleteKeyword = (index) => {
+    const newKeywords = [...videoInfo.keywords];
+    newKeywords.splice(index, 1);
+    setVideoInfo({
+      ...videoInfo,
+      keywords: newKeywords
+    });
+  };
+
+  const handleAddQuestionAnswer = () => {
+    if (currentQuestion.trim() !== "" && currentAnswer.trim() !== "") {
+      setVideoInfo({
+        ...videoInfo,
+        questionsAnswers: [...videoInfo.questionsAnswers, { question: currentQuestion, answer: currentAnswer }]
+      });
+      setCurrentQuestion("");
+      setCurrentAnswer("");
+    }
+  };
+
+  const handleDeleteQuestionAnswer = (index) => {
+    const newQuestionsAnswers = [...videoInfo.questionsAnswers];
+    newQuestionsAnswers.splice(index, 1);
+    setVideoInfo({
+      ...videoInfo,
+      questionsAnswers: newQuestionsAnswers
+    });
+  };
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setVideoInfo({
+  //     ...videoInfo,
+  //     [name]: value
+  //   });
+  // };
 
   const [source, setSource] = useState(null);
   const [video, setVideo] = useState([]);
@@ -110,64 +220,20 @@ const VideoFetch = () => {
     }
   };
 
-  // const handleDelete = (index) => {
-  //   if (video?.length > 0) {
-  //     const updatedApiQual = video.filter((_, xd) => xd !== index);
-  //     setVideo(updatedApiQual);
-  //     deleteVideo(updatedApiQual);
-  //   }
-  // };
-
-  const handlePost = async () => {
-    if (videoInfo.title && videoInfo.link) {
-      setEdit(false);
-
-      const responseJson = await createDoctorVideo(videoInfo);
-      if (responseJson.data.status) {
-        toast.success(responseJson.data.message);
-        getDoctorDetails();
-      } else {
-        toast.error(responseJson.data.message);
-      }
-      setVideoInfo({ link: "", title: "", description: "" });
-      setSource(null);
-    }
-  };
-
-  const deleteVideo = async (id) => {
-    const responseJson = await deleteDoctorVideo(id);
-    if (responseJson.data.status) {
-      toast.success(responseJson.data.message);
-      getDoctorDetails();
-    } else {
-      toast.error(responseJson.data.message);
-    }
-  };
-
-  const updateVideo = async () => {
-    const responseJson = await updateDoctorVideo(videoInfo._id, videoInfo);
-    if (responseJson.data.status) {
-      toast.success(responseJson.data.message);
-      setVideoInfo({
-        link: "",
-        title: "",
-        description: "",
-        category: "",
-      });
-      setEdit(false);
-      getDoctorDetails();
-    } else {
-      toast.error(responseJson.data.message);
-    }
-  };
-
   useEffect(() => {
     getDoctorDetails();
-  }, []);
+    console.log(video);
+  });
+
+  
 
   return (
     <React.Fragment>
       <ThemeProvider theme={theme}>
+        <Typography variant="h5" component={"h5"} className="mb-2" style={{ fontWeight: 'bold' }}>
+          Upload A Video
+        </Typography>
+        <label htmlFor="firstname" style={{ paddingBottom: "5px", fontSize: "18px", paddingTop:"18px" }}>Import URL</label>
         <Stack direction="row" spacing={1} alignItems="center">
           <TextField
             required
@@ -176,7 +242,7 @@ const VideoFetch = () => {
             autoComplete="off"
             name="fetchvideo"
             type="string"
-            placeholder="Import URL*"
+            placeholder="Eg. URL"
             onChange={(e) =>
               setVideoInfo({ ...videoInfo, link: e.target.value })
             }
@@ -186,7 +252,7 @@ const VideoFetch = () => {
               },
             }}
           />
-          <UploadButton onClick={forSubmit}>Fetch</UploadButton>
+          <UploadButton onClick={forSubmit}>Import</UploadButton>
         </Stack>
 
         <Box component={"div"} className="my-2">
@@ -203,7 +269,7 @@ const VideoFetch = () => {
               }}
             />
           )}
-
+          <label htmlFor="firstname" style={{ paddingBottom: "5px", fontSize: "18px", paddingTop:"10px" }}>Video Title</label>
           <TextField
             fullWidth
             autoComplete="off"
@@ -212,14 +278,16 @@ const VideoFetch = () => {
                 height: "2.2em",
               },
             }}
-            placeholder="Video Title*"
+            placeholder="Eg. How to reduce Diabetes"
             value={videoInfo.title}
             onChange={(e) =>
               setVideoInfo({ ...videoInfo, title: e.target.value })
             }
-            sx={{ mt: 2 }}
+            sx={{  }}
           />
-          <FormControl fullWidth sx={{ mt: 2 }}>
+
+          <label htmlFor="firstname" style={{ paddingBottom: "5px", fontSize: "18px", paddingTop:"10px" }}>Category</label>
+          <FormControl fullWidth sx={{ }}>
             <Select
               placeholder="Video Category"
               labelId="demo-simple-select-label"
@@ -238,13 +306,51 @@ const VideoFetch = () => {
                   <MenuItem value={item}>{item}</MenuItem>
                 ))}
             </Select>
+
+          <Grid item xs={12} sm={12} md={12} lg={10}>
+            <label style={{ paddingBottom: "5px", fontSize: "18px", paddingTop:"10px" }}>Keywords</label>
+            <Stack direction="row" spacing={1} alignItems="center">
+            <TextField
+              required
+              fullWidth
+              value={currentKeyword}
+              autoComplete="off"
+              name="degree"
+              type="string"
+              onChange={(e) => setCurrentKeyword(e.target.value)}
+              placeholder="Eg. Fever"
+              InputProps={{
+                sx: {
+                  height: "2.4em",
+                },
+              }}
+            />
+
+              <UploadButton variant="contained" className="px-4" onClick={handleAddKeyword}>
+                Add
+              </UploadButton>
+            </Stack>
+
+            <div style={{ marginTop: "1em" }}>
+              {videoInfo.keywords.map((keyword, index) => (
+                <Chip
+                  key={index}
+                  label={keyword}
+                  onDelete={() => handleDeleteKeyword(index)}
+                  style={{ marginRight: "5px", marginBottom: "5px" }}
+                />
+              ))}
+            </div>
+          </Grid>
+
           </FormControl>
+          <label htmlFor="firstname" style={{ paddingBottom: "5px", fontSize: "18px", paddingTop:"10px" }}>Description</label>
           <TextField
             multiline
             minRows={4}
             fullWidth
             autoComplete="off"
-            placeholder="Description*"
+            placeholder="Eg. "
             value={videoInfo.description}
             onChange={(e) =>
               setVideoInfo((prev) => ({
@@ -252,17 +358,63 @@ const VideoFetch = () => {
                 description: e.target.value,
               }))
             }
-            sx={{ mt: 2 }}
+            sx={{ }}
           />
         </Box>
 
-        <div className="mt-2">
+        <Grid item xs={12} sm={12} md={12} lg={10}>
+        <Typography variant="h5" component="h5" className="mb-2" style={{ fontWeight: 'bold' }}>
+          Important Questions
+        </Typography>
+
+        <label style={{ paddingBottom: "5px", fontSize: "18px", paddingTop: "10px" }}>Question</label>
+        <TextField
+          fullWidth
+          value={currentQuestion}
+          autoComplete="off"
+          name="question"
+          onChange={(e) => setCurrentQuestion(e.target.value)}
+          placeholder="Question"
+          InputProps={{ sx: { height: "2.4em" } }}
+        />
+
+        <label style={{ paddingBottom: "5px", fontSize: "18px", paddingTop: "10px" }}>Answer</label>
+        <TextField
+          fullWidth
+          value={currentAnswer}
+          autoComplete="off"
+          name="answer"
+          onChange={(e) => setCurrentAnswer(e.target.value)}
+          placeholder="Answer"
+          InputProps={{ sx: { height: "2.4em" } }}
+        />
+
+        <Button variant="contained" className="mt-3 px-4" onClick={handleAddQuestionAnswer} style={{ marginTop: "1em" }}>
+          Add Q&A
+        </Button>
+
+        <div style={{ marginTop: "2em" }}>
+          {videoInfo.questionsAnswers.map((qa, index) => (
+            <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+              <Typography style={{ marginRight: '10px' }}>
+                Q: {qa.question} - A: {qa.answer}
+              </Typography>
+              <Button variant="contained" color="secondary" onClick={() => handleDeleteQuestionAnswer(index)}>
+                Delete
+              </Button>
+            </div>
+          ))}
+        </div>
+      </Grid>
+
+        <div className="d-flex justify-content-between mt-3 " style={{paddingTop:"5px" , paddingBottom:"20px" , position:"absolute" , right:"15%" , marginBottom:"1em"}}>
           <UploadButton
             onClick={() => {
               edit ? updateVideo() : handlePost();
             }}
+            style={{ fontFamily: "Montserrat" , backgroundColor:"#133680" , color:"white" , }}
           >
-            {edit ? "Update" : "Post"} Video
+            {edit ? "Update" : "Upload"} Video
           </UploadButton>
         </div>
         {
