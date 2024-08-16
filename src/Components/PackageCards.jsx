4a2b4a049f38d04af8bad5e0240a04bdb148f79f
Priyansh Patel <priyanshpatel741@gmail.com>
2024-08-16 11:25:06 +0530

    newPage/PlansPage

--- src/Components/PackageCards.jsx
+++ src/Components/PackageCards.jsx
@@ -3,14 +3,20 @@ import React from "react";
 import FrequentlyAskedQuestions from "./FrequentlyAskedQuestions";
 import MembershipBenifits from "./MembershipBenifits";
 import Pricing from "./Pricing";
+import Dashboard from "../AdminPannel/pages/Dashboard";
 
 const PackageCards = () => {
   return (
     <>
       <Pricing />
 
-      <MembershipBenifits />
-      <FrequentlyAskedQuestions />
+      <p className="text-center sm:text-[48px] text-[40px] font-poppins font-semibold sm:mt-3 mt-1 capitalize">
+        Our membership benefits
+      </p>
+
+      {/* <MembershipBenifits />
+<FrequentlyAskedQuestions /> */}
+      <Dashboard />
     </>
   );
 };
